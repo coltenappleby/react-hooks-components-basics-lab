3b9e7ef228d40f55c151ca27ff3223c7ff03cfb6
Colten Appleby <colten.appleby@gmail.com>
2021-04-26 16:43:06 -0400

    commit tree 515a69deaa4187a34f04ddf511a37975c6dc2ab5

--- src/components/App.js
+++ src/components/App.js
@@ -16,12 +16,21 @@ function Home() {
   );
 }
 
+function About() {
+  return (
+    <div id="about" >
+      <p> Check out what I am writing in this sentence!</p>
+    </div>
+  )
+
+}
+
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* write an <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
